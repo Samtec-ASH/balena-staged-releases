37a7d87178db7224e8e4e56537191a18e755b9bd
Adam Page <adam.page@samtec.com>
2019-04-04 16:41:18 -0400

    Add preload image to s3 script

--- preload-image.sh
+++ preload-image.sh
@@ -17,7 +17,12 @@ APP_API_KEY=${3:-$APP_API_KEY}
 APP_OS_VERSION=${4:-APP_OS_VERSION}
 S3_BUCKET=${5:-S3_BUCKET}
 
-APP_COMMIT_TAG=${APP_COMMIT:0:7}
+if [ -z "$APP_COMMIT" ]; then echo "Error: APP_COMMIT isnt set"; exit 1; fi 
+if [ -z "$APP_VERSION" ]; then echo "Error: APP_VERSION isnt set"; exit 1; fi 
+if [ -z "$APP_API_KEY" ]; then echo "Error: APP_API_KEY isnt set"; exit 1; fi 
+if [ -z "$APP_OS_VERSION" ]; then echo "Error: APP_OS_VERSION isnt set"; exit 1; fi 
+if [ -z "$S3_BUCKET" ]; then echo "Error: S3_BUCKET isnt set"; exit 1; fi 
+
 BUILD_PATH="/tmp"
 APP_IMG="${APP_NAME}_${APP_DEVICE_TYPE}_v${APP_VERSION}_${APP_COMMIT_TAG}.img"
 APP_IMG_ZIP="${APP_NAME}_${APP_DEVICE_TYPE}_v${APP_VERSION}_${APP_COMMIT_TAG}.zip"
